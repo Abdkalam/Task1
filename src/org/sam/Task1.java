95fa69c1ee8bc7a0c70938c28789e03d4f28ab0a
Abdkalam <amjabdul@gmail.com>
2022-05-05 13:25:10 +0530

    newly added

---
+++ src/org/sam/Task1.java
@@ -0,0 +1,12 @@
+package org.sam;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class Task1 {
+	
+
+	public static int a=10;
+}
+
+
---
+++ src/org/sam/Task2.java
@@ -0,0 +1,14 @@
+
+package org.sam;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Task2 {
+
+	public static void main(String[] args) {
+
+		System.out.println(Task1.a);
+	}
+}
